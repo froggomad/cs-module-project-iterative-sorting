f3285f2818175a1b9bd11c814374ddd2b3514422
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-20 16:03:09 -0700

    added selective and bubble sort. Working on counting sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,36 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
+    # ### MARK TODO FIXME Maybe -1 is not necessary
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        smallest_value = arr[cur_index]
+        
+        for index in range(cur_index, len(arr)):
+            if arr[index] < smallest_value:
+                smallest_value = arr[index]
+                smallest_index = index
         # TO-DO: swap
         # Your code here
-
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    # Your code here
-
-
+def bubble_sort(arr): 
+    count = len(arr) 
+  
+    # Loop through each index except the last one
+    for i in range(count-1):
+        #length - current index - 1
+        #swap positions if there's a mismatch
+        for index in range(0, count - i - 1):
+            if arr[index] > arr[index+1] : 
+                arr[index], arr[index+1] = arr[index+1], arr[index]
     return arr
 
 '''
@@ -40,7 +51,35 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
+    # Your code here    
+    if arr != None:
+        if maximum == None:
+            maximum = len(arr)
+        #make an array of 0's 1 over the max
+        max = maximum+1
+        buckets = [0 for i in range(max)]
+        
+        #fill the buckets given rules
+        for num in arr:
+            if num < 0:
+                return "Error, negative numbers not allowed in Count Sort"
+            if num > maximum:
+                print("Over Max, don't include")
+            else:
+                buckets[num] += 1
+        print(buckets)
+        #track each array position
+        i=0
+        #iterate up to the max, filling each position with a sorted value
+        for bucket in range(max):
+            for _ in range(buckets[bucket]):
+                arr[i] = bucket
+                i += 1
+        return arr
+import random
+arr4 = random.sample(range(200), 50)
+arr = [1,3,3,4,5,6,7,3,4,5,6,7]
+print(sorted(arr4))
+arr = counting_sort(arr4)
 
-
-    return arr
+print(arr)
